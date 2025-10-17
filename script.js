0208cb6adb1b84f0245bc754b47b4f1e0bd228a1
Boricaweb <thanapolsrangkaew@gmail.com>
2025-10-17 18:52:09 +0700

    12th commit add menu apear after click in JS

--- script.js
+++ script.js
@@ -3,9 +3,8 @@
 const introPage = document.querySelector('.intro-page');
 const introLogo = document.querySelector('.intro-logo');
 const contentPage = document.querySelector('.content-page');
-
 //Turn intro page into main page by click the logo image
-function mouseClick() {
+function mouseClickRocket() {
     introPage.style.animation = 'introFadeOut 1s';
     introLogo.style.transform = 'scale(0.7)';
 
@@ -16,9 +15,19 @@ function mouseClick() {
     }
     introPage.addEventListener('animationend', aniEnd);
 }
-
 //Add event to logo mouse click
-introLogo.addEventListener("click", mouseClick);
+introLogo.addEventListener("click", mouseClickRocket);
 
 
 //Content page part//
+//Content-page for Nav bar variables
+const navBar = document.querySelector('nav');
+const buttonMenu = document.querySelector('.button-menu');
+const navMenu = document.querySelector('.nav-menu');
+//The menu appear by click the menu button
+function mouseClickMenu() {
+    navBar.style.backgroundColor = 'rgba(255, 255, 255, 1)';
+    navMenu.style.opacity = '1';
+}
+//Add event to menu mouse click
+buttonMenu.addEventListener("click", mouseClickMenu);
